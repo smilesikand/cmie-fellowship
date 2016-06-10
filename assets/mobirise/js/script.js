8095b355b9682652fc6d8df2f3e225d7871a7b92
Abhishek Gupta <abhishek@Zorroh.local>
2016-06-10 18:30:44 +0530

    added fellowship streams

--- assets/mobirise/js/script.js
+++ assets/mobirise/js/script.js
@@ -420,12 +420,4 @@
 })(jQuery);
 !function() {
 	document.getElementsByClassName('engine')[0].getElementsByTagName('a')[0].removeAttribute('rel');
-
-    if(!document.getElementById('top-1')) {
-        var e = document.createElement("section");
-        e.id = "top-1";
-        e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v2.11.1';
-        document.body.insertBefore(e, document.body.childNodes[0]);
-    }
 }();
